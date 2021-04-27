8b4e2604391dee65e3b506fed59b5eaffae43829
Ankit Kumar Pandit <ankitp@geekyants.com>
2021-04-27 12:55:27 +0530

    turned prop types off

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
   ],
   rules: {
+    'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     'jsx-a11y/anchor-is-valid': [
       'error',
